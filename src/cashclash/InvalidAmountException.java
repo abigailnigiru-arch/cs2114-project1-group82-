4efbc2c1b19a2ee45a4cc3b0cd74d29f1e6be3c7
abigailm07 <abigailnigiru@gmail.com>
2026-09-24 10:10:56 -0400

    Update InvalidAmountException.java

--- src/cashclash/InvalidAmountException.java
+++ src/cashclash/InvalidAmountException.java
@@ -1,7 +1,21 @@
 package cashclash;
 
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author abigailnigiru
+ *  @version Sep 24, 2026
+ */
 public class InvalidAmountException extends Exception {
 
+    // ----------------------------------------------------------
+    /**
+     * Create a new InvalidAmountException object.
+     * @param message
+     */
     public InvalidAmountException(String message) {
         super(message);
     }
